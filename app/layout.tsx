ec45e43a0f84304f3cbfaa712db9d3cdd2727194
ugurakcora <uakcora97@gmail.com>
2023-09-19 19:47:40 +0300

    updated page style

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,10 +18,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider attribute='class' defaultTheme='dark'>
-          {children}
+        <ThemeProvider attribute="class" defaultTheme="dark">
+          <main className="flex min-h-screen flex-col items-center p-24">
+            <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
+              <h2 className="text-2xl text-bold">PokemonFinder</h2>
+            </div>
+            {children}
+          </main>
         </ThemeProvider>
-        </body>
+      </body>
     </html>
+
   )
 }
