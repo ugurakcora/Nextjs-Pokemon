d01a24724818bad42343cfe1ff3fe1078b826751
ugurakcora <uakcora97@gmail.com>
2023-09-20 13:39:00 +0300

    finished project

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -1,6 +1,6 @@
 import { getPokemon } from '@/lib/pokemonAPI';
-import Image from 'next/image';
 import { PokemonImage } from '@/components/pokemon-image';
+import { Progress } from '@/components/ui/progress';
 
 export default async function PokemonPage({ params }:{ params: { pokemonName: string }}) {
   const { pokemonName } = params;
@@ -8,9 +8,24 @@ export default async function PokemonPage({ params }:{ params: { pokemonName: st
   console.log(pokemonObject)
   return (
     <>
-      <h1 className="text-4xl text-bold pt-4">{pokemonName}</h1>
+      <h1 className="text-4xl text-bold pt-4">{pokemonName.charAt(0).toUpperCase()+pokemonName.slice(1)}</h1>
       <div className='m-4' style={{ position: 'relative', width: '300px', height: '300px' }}>
         <PokemonImage image={pokemonObject.sprites.other['official-artwork'].front_default} name={pokemonName} />
+      </div>
+      <h3>Weight: { pokemonObject.weight } </h3>
+      <div className='flex-col'>
+          {
+            pokemonObject.stats.map((statObject: any) => {
+              const statName = statObject.stat.name;
+              const statValue = statObject.base_stat;
+              return (
+                <div className='flex item-stretch' style={{ width: '500px' }} key={statName}>
+                  <h3 className='p-3 w-2/4'>{statName}: {statValue}</h3>
+                  <Progress className='w-2/4 m-auto' value={statValue} />
+                </div>
+              )
+            })
+          }
       </div>
     </>
   )
