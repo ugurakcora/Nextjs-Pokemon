c5edd1664fa9e71123860a5fcd0e9cd95a957f96
ugurakcora <uakcora97@gmail.com>
2023-09-20 12:58:30 +0300

    added search results

--- components/pokemon-grid.tsx
+++ components/pokemon-grid.tsx
@@ -1,45 +1,50 @@
-"use client";
-
+"use client"
+import { PokemonCard } from "./pokemon-card";
 import { useState } from "react";
 import { Label } from "./ui/label";
 import { Input } from "./ui/input";
 
-import { PokemonCard } from "./pokemon-card";
-
 interface PokemonGridProps {
-  pokemonList: any;
+    pokemonList: any
 }
 
-export function PokemonGrid({ pokemonList }: PokemonGridProps) {
-  const [searchText, setSearchText] = useState("");
+export function PokemonGrid({ pokemonList } : PokemonGridProps) {
+    const [ searchText, setSearchText ] = useState("");
 
-  console.log(pokemonList)
+    console.log(pokemonList);
+    const searchFilter = (pokemonList: any) => {
+        return pokemonList.filter(
+            (pokemon: any) => pokemon.name.toLowerCase().includes(searchText.toLowerCase())
+        )
+    }
 
-  return (
-    <>
-      <div>
-        <h3 className="text-2xl py-6 text-center">Search for a Pokemon</h3>
-        <div className="grid w-full max-w-sm items-center gap-1.5">
-          <Label htmlFor="pokemonName">Pokemon Name</Label>
-          <Input
-            type="text"
-            value={searchText}
-            autoComplete="off"
-            id="pokemonName"
-            placeholder="Charizard, Pikachu, etc."
-            onChange={(e) => setSearchText(e.target.value)}
-          />
+    const filteredPokemonList = searchFilter(pokemonList);
+
+    return (
+        <>
+        <div>
+            <h3 className="text-2xl py-6 text-center">Search For Your Pokemon!</h3>
+            <div className="grid w-full max-w-sm items-center gap-1.5">
+                <Label htmlFor="pokemonName">Pokemon Name</Label>
+                <Input 
+                    type="text" 
+                    value={searchText} 
+                    autoComplete="off"
+                    id="pokemonName"
+                    placeholder="Charizard, Pikachu, etc."
+                    onChange={(e) => setSearchText(e.target.value)}
+                />
+            </div>
+            <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
         </div>
-        <h3 className="text-3xl pt-12 pb-6 text-center">Pokemon Collection</h3>
-      </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
-        {
-          pokemonList.map((pokemon: any) => {
-            return <PokemonCard name={pokemon.name} />
-          })
-        }
-      </div>
-    </>
-  );
+        <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-3 lg:text-left">
+            {filteredPokemonList.map((pokemon : any) => {
+                return (
+                    <PokemonCard name={pokemon.name} key={pokemon.name + "Card"}/>
+                )
+            })}
+        </div>
+        </>
+    )
 }
