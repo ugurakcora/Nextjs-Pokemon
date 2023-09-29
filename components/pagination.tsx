a822971a3e385665faf94b0b52bc361c542e5d1f
ugurakcora <uakcora97@gmail.com>
2023-09-30 02:10:50 +0300

    updated active pagination color

--- components/pagination.tsx
+++ components/pagination.tsx
@@ -16,7 +16,7 @@ const Pagination: React.FC<PaginationProps> = ({ totalPages, currentPage, onPage
       pageNumbers.push(
         <button
           key={i}
-          className={`group rounded-lg border border-transparent m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ${currentPage === i ? 'bg-green-900 text-white' : 'bg-gray-300 text-black hover:text-white'}`}
+          className={`group rounded-lg border border-transparent m-3 px-5 py-4 transition-colors dark:border-gray-500 hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 ${currentPage === i ? 'transition-colors text-white' : 'bg-gray-300 text-black hover:text-white'}`}
           onClick={() => onPageChange(i)}
         >
           {i}
